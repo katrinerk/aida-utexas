ce1e65690c13576e7f3ae5285c53e4b3bf56eac6
alex-tomkovich <atomkovich@hotmail.com>
2020-07-12 11:45:45 -0500

    Added wget commands for pretrained model/indexer

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,3 +1,4 @@
-wget https://drive.google.com/file/d/1F1DEBGvpCR7KcVyOVXhRf7DwPxIKb48V/view?usp=sharing
+wget https://drive.google.com/file/d/1uWbppTp1TE4YvX3euVDpH9PnyLp7_sHG/view?usp=sharing -O /aida-utexas/neural_pipeline/gcn2-cuda_best_5000_1.ckpt
+wget https://drive.google.com/file/d/1xQjfWCMrlgiHJvLmEXjUutRFOn_f-Pgu/view?usp=sharing -O /aida-utexas/neural_pipeline/indexers.p
 python neural_pipeline/index.py
 python neural_pipeline/gen_hypoth.py
