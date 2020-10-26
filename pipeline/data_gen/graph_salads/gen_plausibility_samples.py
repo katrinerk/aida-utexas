44c15e750f85ac032fb1d0001a0c3938db8adf4b
alex-tomkovich <atomkovich@hotmail.com>
2020-10-25 22:15:46 -0500

    Various changes

--- pipeline/data_gen/graph_salads/gen_plausibility_samples.py
+++ pipeline/data_gen/graph_salads/gen_plausibility_samples.py
@@ -15,147 +15,110 @@ Usage:
     
 """
 import dill
-from utils import *
-from itertools import chain, combinations
-import random as rd
-import sys
+import random
 import os
 from tqdm import tqdm
+from collections import defaultdict
+import argparse
 
-def gen_1_neg_sample(g0, g1, g2):
-    while True:
-        n_g0 = rd.randint(0, len(g0))
-        n_g1 = rd.randint(0, len(g1))
-        n_g2 = rd.randint(0, len(g2))
-        # if sum([n_g0, n_g1, n_g2]) != n_g0 and sum([n_g0, n_g1, n_g2]) != n_g1 and sum([n_g0, n_g1, n_g2]) != n_g2:
-        if (n_g0 == 0 and n_g1*n_g2 == 0) or (n_g1 == 0 and n_g2*n_g0 == 0) or (n_g2 == 0 and n_g1*n_g0 == 0):
-            print('lens:', len(g0), len(g1), len(g2))
-            print('n_gx:', n_g0, n_g1, n_g2)
-            continue
-        else:
-            break
-    neg_sample = rd.sample(g0, n_g0) + rd.sample(g1, n_g1) + rd.sample(g2, n_g2)
-    return neg_sample
+def gen_neg_sample(core_graph_ids, graph_id_to_stmt_map, used_clusters):
+    dup = True
 
-def gen_neg_samples_faster(g0, g1, g2):
-    neg_samples = [gen_1_neg_sample(g0, g1, g2), gen_1_neg_sample(g0, g1, g2), gen_1_neg_sample(g0, g1, g2)]
-    return neg_samples
+    while dup:
+        stmt_ids = set()
 
-def get_stmt_between_eres(graph_mix, ere1, ere2):
-    stmts = list(set.intersection(graph_mix.eres[ere1].stmt_ids, graph_mix.eres[ere2].stmt_ids))
-    return stmts[0]
+        for graph_id in core_graph_ids:
+            stmt_pool = graph_id_to_stmt_map[graph_id]
+            rand_int = random.randint(1, len(stmt_pool))
 
+            stmt_ids = set.union(stmt_ids, set(random.sample(list(stmt_pool), rand_int)))
 
-# Filename
-input_dir = sys.argv[1]
-output_dir = sys.argv[2]
+        if stmt_ids not in used_clusters:
+            dup = False
 
-# Create output directory if it does not exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    return stmt_ids
 
-# List all graph salads in the input_dir
-salad_fname_list = os.listdir(input_dir)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_dir", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, required=True)
+    parser.add_argument("--num_neg_smp_per_merge_pt", type=int, default=3)
 
-# Counts for no src_graphs / src_graph_stmt
-cnt_no_src = 0
-cnt_no_src_stmt = 0
+    args = parser.parse_args()
+    locals().update(vars(args))
 
-# Loop over all graph salads
-for salad_fname in tqdm(salad_fname_list):
-    print('Input: {}'.format(salad_fname))
+    # Create output directory if it does not exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    # Load graph salad
-    graph_dict = dill.load(open(os.path.join(input_dir, salad_fname), 'rb'))
-    graph_mix = graph_dict['graph_mix']
-    ere_mat_ind = graph_dict['ere_mat_ind']
-    query_ere_ids = {ere_mat_ind.get_word(ere_ind) for ere_ind in graph_dict['query_eres']}
-    origin_id = [ere_id for ere_id in query_ere_ids if graph_dict['graph_mix'].eres[ere_id].category == 'Event'][0]
+    data_groups = ['Train', 'Val', 'Test']
 
-    # Graph ids (of three subgraphs)
-    graph_ids = [graph_dict['target_graph_id']] + list({graph_mix.stmts[stmt_id].graph_id for stmt_id in graph_mix.eres[origin_id].stmt_ids} - {graph_dict['target_graph_id']})
-    graph_ids_dict = dict(zip(graph_ids, [0, 1, 2]))
-    query_ere_ids = {ere_mat_ind.get_word(ere_ind) for ere_ind in graph_dict['query_eres']}
+    [os.makedirs(os.path.join(output_dir, data_group)) for data_group in data_groups if not os.path.exists(os.path.join(output_dir, data_group))]
 
-    # Find merge points
-    merge_ere_ids = set()
-    for ere_id, ere in graph_mix.eres.items():
-        if len({graph_mix.stmts[stmt_id].graph_id for stmt_id in ere.stmt_ids if graph_mix.stmts[stmt_id].graph_id not in graph_dict}) > 1:
-            merge_ere_ids.add(ere_id)
-    merge_ere_ids = list(merge_ere_ids)
+    for data_group in data_groups:
+        input_path = os.path.join(input_dir, data_group)
 
-    # Generate positive and negative samples for plausibility classifier for hypothesis seeds
-    pos_samples = dict()
-    neg_samples = dict()
+        # List all graph salads in the input_path
+        salad_fname_list = os.listdir(input_path)
 
-    for m in range(len(merge_ere_ids)):  # loop over merge points
-        merge_m = merge_ere_ids[m]
-        # print("\nMerge point: {} ({})".format(graph_mix.eres[merge_m].label[0], graph_mix.eres[merge_m].category))
-        merge_m_neigh = graph_mix.eres[merge_m].neighbor_ere_ids
+        # Loop over all graph salads
+        for salad_fname in tqdm(salad_fname_list):
+            # Load graph salad
+            graph_dict = dill.load(open(os.path.join(input_path, salad_fname), 'rb'))
+            graph_mix = graph_dict['graph_mix']
+            ere_mat_ind = graph_dict['ere_mat_ind']
+            query_ere_ids = {ere_mat_ind.get_word(ere_ind) for ere_ind in graph_dict['query_eres']}
+            origin_id = [ere_id for ere_id in query_ere_ids if graph_dict['graph_mix'].eres[ere_id].category == 'Event'][0]
 
-        # Initialize neighbor sets of each source graph
-        neigh_g0 = set()
-        neigh_g1 = set()
-        neigh_g2 = set()
+            # Graph ids (of subgraphs)
+            core_graph_ids = [graph_dict['target_graph_id']] + list({graph_mix.stmts[stmt_id].graph_id for stmt_id in graph_mix.eres[origin_id].stmt_ids} - {graph_dict['target_graph_id']})
 
-        # For each neighbor around a merge point
-        for n in range(len(merge_m_neigh)):
-            neighbor_n = list(merge_m_neigh)[n]
-            try:
-                src_graph = graph_ids_dict[graph_mix.eres[neighbor_n].graph_id]
-            except KeyError:
-                cnt_no_src += 1
-                print('src_graph: No such graph in the dict!')
-                continue
+            # Find merge points
+            merge_ere_ids = set()
 
-            stmt_between = get_stmt_between_eres(graph_mix, merge_m, neighbor_n)
-            try:
-                src_graph_stmt = graph_ids_dict[graph_mix.stmts[stmt_between].graph_id]
-            except KeyError:
-                cnt_no_src_stmt += 1
-                print('src_graph_stmt: No such graph in the dict!')
-                continue
+            for ere_id, ere in graph_mix.eres.items():
+                if len(set.intersection({graph_mix.stmts[stmt_id].graph_id for stmt_id in ere.stmt_ids}, set(core_graph_ids))) == len(core_graph_ids):
+                    merge_ere_ids.add(ere_id)
 
-            # Classify the neighboring statement by source graph
-            if src_graph_stmt == 0:
-                neigh_g0.add(stmt_between)
-            elif src_graph_stmt == 1:
-                neigh_g1.add(stmt_between)
-            elif src_graph_stmt == 2:
-                neigh_g2.add(stmt_between)
+            assert not set.intersection(merge_ere_ids, graph_dict['noisy_merge_points'])
 
-        # Convert to lists for easier indexing
-        neigh_g0 = list(neigh_g0)
-        neigh_g1 = list(neigh_g1)
-        neigh_g2 = list(neigh_g2)
+            # Generate positive and negative samples for plausibility classifier for hypothesis seeds
+            pos_samples = defaultdict(list)
+            neg_samples = defaultdict(list)
 
-        if len(neigh_g0) == 0 or len(neigh_g1) == 0 or len(neigh_g2) == 0:
-            continue
+            for merge_ere_id in merge_ere_ids:  # loop over merge points
+                graph_id_to_stmt_map = dict()
 
-        print('Ready to generate samples!')
+                for core_graph_id in core_graph_ids:
+                    graph_id_to_stmt_map[core_graph_id] = {stmt_id for stmt_id in graph_mix.eres[merge_ere_id].stmt_ids if graph_mix.stmts[stmt_id].tail_id and graph_mix.stmts[stmt_id].graph_id == core_graph_id}
 
-        # Generate three positive samples
-        # Do not use merge point with only one argument from a source
-        pos_samples_list = []
-        for neigh_gx in [neigh_g0, neigh_g1, neigh_g2]:
-            if len(neigh_gx) > 1:
-                pos_samples_list.append(neigh_gx)
-        pos_samples[merge_m] = pos_samples_list
+                # Generate three positive samples
+                # Do not use merge point with only one argument from a source
+                pos_samples[merge_ere_id] = [value for value in graph_id_to_stmt_map.values() if len(value) > 1]
 
-        print('Positive finished')
+                used_clusters = set()
 
-        # Generate negative samples using the existing neighbors from different sources
-        neg_samples[merge_m] = gen_neg_samples_faster(neigh_g0, neigh_g1, neigh_g2)
-        print('Negative finished')
+                # Generate negative samples using the existing neighbors from different sources
+                for _ in range(num_neg_smp_per_merge_pt):
+                    neg_cluster = gen_neg_sample(core_graph_ids, graph_id_to_stmt_map, used_clusters)
 
-    graph_dict["pos_samples"] = pos_samples
-    graph_dict["neg_samples"] = neg_samples
+                    neg_samples[merge_ere_id].append(neg_cluster)
+                    used_clusters.add(frozenset(neg_cluster))
 
-    # Save pickled graph
-    dill.dump(graph_dict, open(os.path.join(output_dir, salad_fname), "wb"))
+                for sample_iter in range(len(pos_samples[merge_ere_id])):
+                    ere_ids = set.union(*[{graph_mix.stmts[stmt_id].head_id, graph_mix.stmts[stmt_id].tail_id} for stmt_id in pos_samples[merge_ere_id][sample_iter]])
 
-    # Save memory by deleting variables
-    del graph_dict, graph_mix, graph_ids, graph_ids_dict, merge_ere_ids, pos_samples, neg_samples, neighbor_n, neigh_g0, neigh_g1, neigh_g2, ere_mat_ind, query_ere_ids
+                    for ere_id in ere_ids:
+                        pos_samples[merge_ere_id][sample_iter].update({stmt_id for stmt_id in graph_mix.eres[ere_id].stmt_ids if not graph_mix.stmts[stmt_id].tail_id})
+
+                for sample_iter in range(len(neg_samples[merge_ere_id])):
+                    ere_ids = set.union(*[{graph_mix.stmts[stmt_id].head_id, graph_mix.stmts[stmt_id].tail_id} for stmt_id in neg_samples[merge_ere_id][sample_iter]])
+
+                    for ere_id in ere_ids:
+                        neg_samples[merge_ere_id][sample_iter].update({stmt_id for stmt_id in graph_mix.eres[ere_id].stmt_ids if not graph_mix.stmts[stmt_id].tail_id})
+
+            # Save pickled graph
+            dill.dump({'pos_samples': pos_samples, 'neg_samples': neg_samples}, open(os.path.join(output_dir, data_group, salad_fname), "wb"))
 
 
 # # --------------------------------------------------------------------------------------------------------------
