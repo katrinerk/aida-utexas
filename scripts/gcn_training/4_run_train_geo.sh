21ca6283ba6d5614287a4aabf4131b58c8a08451
Chameleon Cloud User <cc@test4.novalocal>
2023-05-05 14:58:53 +0000

    update param

--- scripts/gcn_training/4_run_train_geo.sh
+++ scripts/gcn_training/4_run_train_geo.sh
@@ -3,7 +3,7 @@ echo $root
 src=/home/cc/aida-utexas 
 trainer="$src/pipeline/training/graph_salads/trainer.py"
 name="geo_all"
-logdir="/home/cc/tensorboard/$name
-"
+logdir="/home/cc/tensorboard/$name"
 indexer="$root/indexers_geo.p"
-PYTHONPATH=$src python $trainer --device 1 --data_dir $root --valid_dir "Val" --save_path "$root/model_$name" --indexer_info_file $indexer --hidden_size 302 --attention_size 302 --init_prob_force 0.25 --force_decay 0.9659 --weight_decay 0.001 --batch_size 25 --num_epochs 4 --force_every 3000 --log_dir $logdir --use_highest_ranked_gold --force --self_attend --attn_head_stmt_tail --valid_every 500 --load_path "$root/model_geo_more/gcn2-cuda_best_9000_0_0.69.ckpt"
+PYTHONPATH=$src python $trainer --device 1 --data_dir $root --valid_dir "Val" --save_path "$root/model_$name" --indexer_info_file $indexer --hidden_size 300 --attention_size 300 --init_prob_force 0.25 --force_decay 0.9659 --weight_decay 0.001 --batch_size 25 --num_epochs 4 --force_every 3000 --log_dir $logdir --use_highest_ranked_gold --force --self_attend --attn_head_stmt_tail --valid_every 500 
+# --load_path "$root/model_geo_more/gcn2-cuda_best_9000_0_0.69.ckpt"
